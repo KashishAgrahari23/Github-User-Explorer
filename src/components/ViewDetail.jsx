72a416e9492f072fd4184eabfdc3c8bc01681d42
KashishAgrahari23 <kashishagrahari23@gmail.com>
2025-10-30 23:46:15 +0530

    routing done

--- src/components/ViewDetail.jsx
+++ src/components/ViewDetail.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
 const ViewDetail = (user) => {
-    
+    const [res,setRes] = useState([])
+  useEffect(
+    (user) => {
+      let data = fetch(`https://api.github.com/users/${user}/repos`);
+      let res = data.json();
+      console.log(res.data);
+      setRes(res.data)
+    },
+    [user]
+  );
+
   return (
     <div>
       <h1>detail page</h1>
-      
+      {res.map((elem) => {
+        return (
+          <div>
+            <h3>{elem.name}</h3>
+            <strong>{elem.stargazers_count}</strong>
+            <p>{elem.description}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ViewDetail
+export default ViewDetail;
